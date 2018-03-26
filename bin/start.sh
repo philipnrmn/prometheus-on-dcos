2d8f19af0e39613221f19111a60c39782d81c25f
Philip Norman <philip@mesosphere.io>
2018-03-26 16:24:40 -0700

    Add / route prefix for reverse proxying

--- bin/start.sh
+++ bin/start.sh
@@ -4,5 +4,5 @@
 start-stop-daemon -S -b -x /bin/configure
 
 # Run Prometheus as the main entrypoint
-/bin/prometheus "--config.file=/etc/prometheus/prometheus.yml" "--web.console.libraries=/etc/prometheus/console_libraries" "--web.console.templates=/etc/prometheus/consoles"
+/bin/prometheus "--config.file=/etc/prometheus/prometheus.yml" "--web.console.libraries=/etc/prometheus/console_libraries" "--web.console.templates=/etc/prometheus/consoles" "--web.route-prefix=/"
 
